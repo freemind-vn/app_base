2c059b72d9890d61f26594ea961b8f7e5b6b58a4
Vinh <thanh.vinh@hotmail.com>
2024-08-28 15:26:03 +0700

    refactor(cli): `create` -> `init`
    fix(cli): setup new project

--- bin/app_base.dart
+++ bin/app_base.dart
@@ -1,11 +1,9 @@
 import 'package:args/command_runner.dart';
 
-import 'cmd/create_cmd.dart';
-import 'cmd/update_cmd.dart';
+import 'cmd/init_cmd.dart';
 
 void main(List<String> args) {
   CommandRunner('app_base', "A Freemind's simple app template")
-    ..addCommand(CreateCommand())
-    ..addCommand(UpdateCommand())
+    ..addCommand(InitCommand())
     ..run(args);
 }
--- bin/cmd/init_cmd.dart
+++ bin/cmd/init_cmd.dart
@@ -1,36 +1,30 @@
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:io/io.dart';
 import 'package:path/path.dart';
 
 import 'cmd.dart';
 
-const gitRepoUrl = 'https://github.com/freemind-vn/app_base.git';
-
-// Index generator file config
-const indexGenerator = '''
-index_generator:
-  exclude:
-    - "**.g.dart"
-  indexes:
-    - path: lib
-      include: [src/*.dart]
-      name: app
-''';
-
-class CreateCommand extends Command {
+class InitCommand extends Command {
   @override
-  final name = 'create';
+  final name = 'init';
 
   @override
-  final description = 'Create a new project';
+  final description = 'Setup a new project';
 
-  CreateCommand() {
+  InitCommand() {
     argParser.addOption(
       'version',
       abbr: 'v',
       help: 'The specific version',
       defaultsTo: 'main',
+    );
+
+    argParser.addOption(
+      'source',
+      abbr: 's',
+      help: 'The path of the source code',
     );
   }
 
@@ -38,16 +32,27 @@ class CreateCommand extends Command {
   // [run] may also return a Future.
   void run() async {
     final version = argResults?.option('version');
-    // Clone the repo
+
     final tmpDir = Directory.systemTemp.createTempSync();
-    await execShell(
-        'git clone -b $version --depth 1 --progress $gitRepoUrl ${tmpDir.path}');
+
+    final source = argResults?.option('source');
+
+    // If use the source code path
+    if (source != null) {
+      await copyPath(source, tmpDir.path);
+    } else {
+      // Clone the repo
+      await execShell(
+        'git clone -b $version --depth 1 --progress $gitRepoUrl ${tmpDir.path}',
+      );
+    }
 
     // Copy example/Makefile
     final examplePath = Directory(join(tmpDir.path, 'example')).path;
     Future.wait([
       copyFile(join(examplePath, 'Makefile')),
       copyFile(join(examplePath, 'Dockerfile')),
+      copyFile(join(examplePath, '.editorconfig')),
       // copyFile(join(examplePath, '.vscode/settings.json'), '.vscode/settings.json'),
     ]);
 
@@ -62,15 +67,20 @@ class CreateCommand extends Command {
 
     // Create widgetbook
     await execShell('flutter create --empty widgetbook');
-    await execShell('flutter pub add widgetbook widgetbook_annotation');
     await execShell(
-        'flutter pub add dev:widgetbook_generator dev:build_runner');
+      'dart pub add widgetbook widgetbook_annotation',
+      workingDirectory: 'widgetbook',
+    );
+    await execShell(
+      'dart pub add dev:widgetbook_generator dev:build_runner',
+      workingDirectory: 'widgetbook',
+    );
   }
 
   void _createIndexGeneratorConfig() async {
     final file = File('index_generator.yaml');
     if (!await file.exists()) {
-      file.writeAsString(indexGenerator);
+      file.writeAsString(indexGeneratorTpl);
     }
   }
 }
